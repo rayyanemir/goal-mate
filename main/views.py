7c21af38aeb016253609585a0aabb6ae4692fe64
Rayyan Emir Muhammad <rayyan.emir@ui.ac.id>
2025-09-06 22:07:40 +0700

    Tugas 2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Jersey FC Barcelona X Travis Scott (Official)',
+        'price': '1200000',
+        'description': 'Jersey Barcelona yang berkolaborasi dengan Travis Scott',
+        'thumbnail': 'https://share.google/be6yjHPUv0jePt1yr',
+        'category': 'Jersey',
+        'is_featured': 'Yes',
+    }
+
+    return render(request, "main.html", context)
