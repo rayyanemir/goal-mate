f2d53a37d2fa37ffe17d34b965655aa05b3ae197
Rayyan Emir Muhammad <rayyan.emir@ui.ac.id>
2025-09-13 15:57:10 +0700

    update tugas 3

--- GoalMate/settings.py
+++ GoalMate/settings.py
@@ -32,7 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","rayyan-emir-goalmate.pbp.cs.ui.ac.id"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://rayyan-emir-goalmate.pbp.cs.ui.ac.id"
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'GoalMate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
